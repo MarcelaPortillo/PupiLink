5ba9b55d5f69f978f128190400806e77461cf0b4
ken13-spect <00120620@uca.edu.sv>
2024-07-08 08:30:22 -0600

    :lipstick: Update design on My Ads and My Requests pages

--- client/src/pages/MyAds.tsx
+++ client/src/pages/MyAds.tsx
@@ -74,32 +74,44 @@ const MyAds: React.FC = () => {
     }
   };
 
+  const lodgingStatusMap = {
+  AVAILABLE: 'Disponible',
+  BOOKED: 'Reservado',
+  RENTED: 'Alquilado',
+  UNAVAILABLE: 'No disponible',
+  UNDER_MAINTENANCE: 'En mantenimiento',
+};
+
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 p-4 mt-20">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold">Mis Anuncios</h2>
-        <Link to="/publish" className="bg-custom-purple text-white px-4 py-2 rounded hover:bg-hover_colors">
+        <h2 className="text-2xl font-bold text-gray-500">Mis Anuncios</h2>
+        <Link to="/publish" className="bg-custom-purple text-white font-bold font-barlowc px-4 py-2 rounded hover:bg-hover_colors">
           Crear Anuncio
         </Link>
       </div>
       {lodgings.map((lodging) => (
-        <div key={lodging.id} className="bg-white p-4 rounded-lg shadow-md mb-4 flex items-center">
+        <div key={lodging.id} className="bg-gray-200 p-4 rounded-lg shadow-md mb-4 flex items-center">
           <img
             src={pb.files.getUrl(lodging, lodging.image)}
             alt={lodging.title}
-            className="w-20 h-20 object-cover rounded-md mr-4"
+            className="w-24 h-20 object-cover rounded-md mr-4"
           />
-          <div className="flex-1">
-            <h3 className="font-bold text-custom-purple mb-2">{lodging.title}</h3>
-            <p className="text-gray-600">Estado: {lodging.status}</p>
-            <p className="text-gray-600">Precio: ${lodging.price}</p>
+          <div className="flex-grow flex justify-between items-center">
+            <div className="flex flex-col">
+              <h3 className="font-bold text-gray-600 font-barlowc">{lodging.title}</h3>
+              <div className="flex space-x-4">
+              <p className="text-gray-600">Estado: {lodgingStatusMap[lodging.status]}</p>
+                <p className="text-gray-600 font-barlowc">Precio: ${lodging.price}</p>
+              </div>
+            </div>
+            <button
+              onClick={() => handleDelete(lodging.id)}
+              className="bg-red-500 text-white px-4 py-2 font-bold font-barlowc rounded hover:bg-red-700"
+            >
+              Eliminar
+            </button>
           </div>
-          <button
-            onClick={() => handleDelete(lodging.id)}
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
-          >
-            Eliminar
-          </button>
         </div>
       ))}
       <ToastContainer />
--- client/src/pages/MyRequests.tsx
+++ client/src/pages/MyRequests.tsx
@@ -21,6 +21,8 @@ const MyRequests: React.FC = () => {
   const [requests, setRequests] = useState<Request[]>([]);
   const [lodgings, setLodgings] = useState<{ [key: string]: Lodging }>({});
 
+  
+
   useEffect(() => {
     const fetchRequests = async () => {
       try {
@@ -58,40 +60,55 @@ const MyRequests: React.FC = () => {
     try {
       await pb.collection('lodgingRequest').delete(requestId);
       setRequests(requests.filter(request => request.id !== requestId));
-      toast.success('Solicitud cancelada');
+      toast.success('Solicitud cancelada exitosamente');
     } catch (error) {
       console.error('Error canceling request:', error);
       toast.error('Error al cancelar la solicitud');
     }
   };
 
+  const requestStatusMap = {
+    SENT: "Enviado",
+    ACCEPTED: "Aceptado",
+    REJECTED: "Rechazado",
+    ON_EVALUATION: "En evaluación",
+  };
+
   return (
-    <div className="container mx-auto px-4 mt-20">
-      <h1 className="text-2xl font-bold my-4">Tus Solicitudes</h1>
+    <div className="container mx-auto pt-4 mt-20 bg-gray-100">
+      <h1 className="text-2xl font-bold text-gray-500 my-4">Tus Solicitudes</h1>
       <div className="space-y-4">
-        {requests.map(request => {
+        {requests.map((request) => {
           const lodging = lodgings[request.lodging];
           return (
-            <div key={request.id} className="bg-gray-100 p-4 rounded-lg flex items-center justify-between">
+            <div key={request.id} className="bg-gray-200 p-4 m-4 rounded-lg flex items-center justify-between">
               <div className="flex items-center">
                 {lodging && (
-                  <img src={pb.files.getUrl(lodging, lodging.image)} alt={lodging.title} className="h-24 w-24 rounded-lg mr-4 object-cover" />
+                  <img
+                    src={pb.files.getUrl(lodging, lodging.image)}
+                    alt={lodging.title}
+                    className="h-24 w-25   rounded-lg mr-4 object-cover"
+                  />
                 )}
                 <div>
-                  <div className="text-purple-600 font-bold">{lodging?.title || 'Ubicación desconocida'}</div>
-                  <div>Fecha de Creación: {new Date(request.created).toLocaleDateString()}</div>
-                  <div>Precio Propuesto: ${request.proposedPrice}</div>
-                  <div>Estado: {request.status}</div>
+                  <div className="text-gray-600  text-lg font-bold">
+                    {lodging?.title || "Ubicación desconocida"}
+                  </div>
+                  <div className="flex space-x-4">
+                    <div>Fecha de Creación: {new Date(request.created).toLocaleDateString()}</div>
+                    <div>Precio Propuesto: ${request.proposedPrice}</div>
+                    <div>
+                      Estado: {requestStatusMap[request.status] || request.status}
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-1">
                 <button
                   onClick={() => handleCancelRequest(request.id)}
-                  className="bg-red-500 text-white p-2 rounded-full hover:bg-red-600"
+                  className="bg-red-500 text-white px-4 py-2 mr-5 font-bold font-barlowc rounded hover:bg-red-700"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.707a1 1 0 00-1.414-1.414L10 8.586 7.707 6.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z" clipRule="evenodd" />
-                  </svg>
+                  Eliminar
                 </button>
               </div>
             </div>
